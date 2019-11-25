f662814b53e274db7e529632a134ef36f39d84db
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-25 16:35:19 +0100

    Add footer
    Add footer with copyright notice and links to legal notice and privacy policy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Fragment } from 'react';
+import Footer from './components/Footer';
 
 export default function App() {
-  return <Fragment>Hello World!</Fragment>;
+  return (
+    <Fragment>
+      <Footer />
+    </Fragment>
+  );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+export default function Footer() {
+  return (
+    <Fragment>
+      <footer>
+        <p>&copy; Peter R. Stuhlmann, 2019</p>
+        <div>
+          <a href="#">Impressum</a>
+          <a href="#">Datenschutzerklärung</a>
+        </div>
+      </footer>
+    </Fragment>
+  );
+}
