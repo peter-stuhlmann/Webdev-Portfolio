31eb51c46aec1f72d6c854eb158fba2302dc1358
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-25 18:23:03 +0100

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import LegalNotice from './components/LegalNotice';
+import PrivacyPolicy from './components/PrivacyPolicy';
 
 export default function App() {
   return (
-    <Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/impressum" component={LegalNotice} />
+        <Route path="/datenschutzerklaerung" component={PrivacyPolicy} />
+      </Switch>
       <Footer />
-    </Fragment>
+    </BrowserRouter>
   );
 }
