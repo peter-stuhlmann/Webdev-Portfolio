ca7e42b556d6ac6afcb685ac8a72e5719f37fb35
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-26 17:21:13 +0100

    Add a button to visit a live demo

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -10,6 +10,9 @@ export default function PortfolioItem() {
           <h3>Lorem ipsum</h3>
           <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
         </div>
+        <a href="#" className="button">
+          Live Demo
+        </a>
       </div>
     </StyledPortfolioItem>
   );
@@ -43,6 +46,60 @@ const StyledPortfolioItem = styled.section`
 
       p {
         transition: all 0.5s cubic-bezier(0.4, 0, 0.2, 1) 0.2s;
+      }
+    }
+
+    .button {
+      position: absolute;
+      right: 10%;
+      padding: 5px 8px;
+      transition: color 0.5s cubic-bezier(0.4, 0, 0.2, 1) 0.2s;
+      text-decoration: none;
+      color: #000;
+
+      &:before {
+        position: absolute;
+        content: '';
+        height: 20px;
+        width: 20px;
+        border-style: solid;
+        border-width: 0;
+        transition-property: height, width, border-color;
+        transition-duration: 0.4s, 0.4s, 0.5s;
+        transition-timing-function: ease, ease,
+          cubic-bezier(0.4, 0, 0.2, 1) 0.2s;
+        transition-delay: 0s, 0s, 0.25s;
+        border-color: #000;
+        border-left-width: 1px;
+        border-top-width: 1px;
+        left: -7px;
+        top: -6px;
+      }
+
+      &:after {
+        position: absolute;
+        content: '';
+        height: 21px;
+        width: 20px;
+        border-color: transparent;
+        border-style: solid;
+        border-width: 0;
+        transition-property: height, width, border-color;
+        transition-duration: 0.4s, 0.4s, 0.5s;
+        transition-timing-function: ease, ease, cubic-bezier(0.4, 0, 0.5, 1);
+        transition-delay: 0s, 0s, 0.25s;
+        border-bottom-width: 1px;
+        border-color: #000;
+        border-right-width: 1px;
+        bottom: -5px;
+        right: -5px;
+      }
+
+      &:hover:before,
+      &:hover:after {
+        cursor: pointer;
+        height: calc(100% + 10px);
+        width: calc(100% + 10px);
       }
     }
   }
