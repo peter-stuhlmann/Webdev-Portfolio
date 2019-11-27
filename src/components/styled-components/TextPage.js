813134a58d556291c53510b7a646f640929dfe23
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-11-27 16:20:49 +0100

    Add legal notice and privacy policy texts and style them

---
+++ src/components/styled-components/TextPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTextPage = styled.section`
+  h1 {
+    font-size: 3em;
+  }
+
+  h2 {
+    text-align: left;
+    margin-top: 75px;
+  }
+`;
+
+export const TextPage = props => (
+  <StyledTextPage>{props.children}</StyledTextPage>
+);
