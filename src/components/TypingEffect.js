88676a85a0bc5fba9d0780e7c0caacd0a6f41115
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-30 04:32:00 +0200

    Use react-typist for the typing effect

--- src/components/TypingEffect.js
+++ src/components/TypingEffect.js
@@ -1,40 +1,53 @@
-import React, { useContext } from 'react';
-import styled, { keyframes } from 'styled-components';
+import React, { Fragment, useContext } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Typist from 'react-typist';
 
 import { Context } from '../Context';
 
 export default function TypingEffect() {
   const { content } = useContext(Context);
 
-  return <StyledTypingEffect>{content.header.heading}</StyledTypingEffect>;
+  return (
+    <Fragment>
+      <TypingEffectStyles />
+      <Typist className="typing-effect">
+        {content.header.heading.pre} {content.header.heading.word[0]}
+        <Typist.Delay ms={1500} />
+        <Typist.Backspace
+          count={content.header.heading.word[0].length}
+          delay={1000}
+        />
+        <Typist.Delay ms={750} />
+        {content.header.heading.word[1]}
+      </Typist>
+    </Fragment>
+  );
 }
 
-const animatedText = keyframes`
-  from {
-    width: 0;
-  }
-  to {
-    width: 550px;
-  }
-`;
+const TypingEffectStyles = createGlobalStyle`
+  .typing-effect {
+    font-family: 'WebDevFont SourceCode', monospace;
+    font-size: 50px;
+    color: #fff;
+    margin: 0 auto;
 
-const animatedCursor = keyframes`
-  from {
-    border-right-color: #fff;
+    @media (max-width: 1250px) {
+      font-size: 30px;
+    }  
   }
-  to {
-    border-right-color: transparent;
-  }
-`;
 
-const StyledTypingEffect = styled.h1`
-  border-right: solid 3px #fff;
-  white-space: nowrap;
-  overflow: hidden;
-  font-family: 'WebDevFont SourceCode', monospace;
-  font-size: 50px;
-  color: #fff;
-  margin: 0 auto;
-  animation: ${animatedText} 2s steps(19, end) 1s 1 normal both,
-    ${animatedCursor} 700ms steps(19, end) infinite;
+  .Cursor {
+    display: inline-block;
+  
+    &--blinking {
+      opacity: 1;
+      animation: blink 0.6s linear infinite;
+
+      @keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+    }
+  }
 `;
