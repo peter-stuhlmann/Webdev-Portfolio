13a60cc004b1faa018c10e696aa64932d8074381
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-30 20:43:05 +0200

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Suspense, useContext } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import LegalNotice from './components/LegalNotice';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import NotFound from './components/NotFound';
+
+import { Context } from './Context';
 import { GlobalStyles } from './components/styled-components/GlobalStyles';
-import ScrollToTop from './components/ScrollToTop';
-import MainNavigation from './components/MainNavigation/MainNavigation';
+
+const Footer = React.lazy(() => import('./components/Footer'));
+const Home = React.lazy(() => import('./components/Home'));
+const LegalNotice = React.lazy(() => import('./components/LegalNotice'));
+const PrivacyPolicy = React.lazy(() => import('./components/PrivacyPolicy'));
+const NotFound = React.lazy(() => import('./components/NotFound'));
+const ScrollToTop = React.lazy(() => import('./components/ScrollToTop'));
+const MainNavigation = React.lazy(() =>
+  import('./components/MainNavigation/MainNavigation')
+);
 
 export default function App() {
+  const { content } = useContext(Context);
+
   return (
     <BrowserRouter>
-      <MainNavigation />
-      <ScrollToTop />
-      <GlobalStyles />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/impressum" component={LegalNotice} />
-        <Route path="/datenschutzerklaerung" component={PrivacyPolicy} />
-        <Route component={NotFound} />
-      </Switch>
-      <Footer />
+      <Suspense fallback={<div>{content.loading}</div>}>
+        <MainNavigation />
+        <ScrollToTop />
+        <GlobalStyles />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/impressum" component={LegalNotice} />
+          <Route path="/datenschutzerklaerung" component={PrivacyPolicy} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 }
