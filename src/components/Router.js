a122a0346349fe171c9a0cc008fa6b6693fdcb98
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-31 03:09:43 +0200

    Integrate GoogleAnalytics tracking

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import React, { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { ga } from '../helpers/analytics';
+
 const Home = lazy(() => import('./Home'));
 const LegalNotice = lazy(() => import('./LegalNotice'));
 const PrivacyPolicy = lazy(() => import('./PrivacyPolicy'));
@@ -9,10 +11,10 @@ const NotFound = lazy(() => import('./NotFound'));
 export default function Router() {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/impressum" component={LegalNotice} />
-      <Route path="/datenschutzerklaerung" component={PrivacyPolicy} />
-      <Route component={NotFound} />
+      <Route exact path="/" component={ga(Home)} />
+      <Route path="/impressum" component={ga(LegalNotice)} />
+      <Route path="/datenschutzerklaerung" component={ga(PrivacyPolicy)} />
+      <Route component={ga(NotFound)} />
     </Switch>
   );
 }
