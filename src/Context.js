a93883a847bf9eff9eb8a41d61ee735531fbf900
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-05-24 19:42:54 +0200

    Save language with local storage

--- src/Context.js
+++ src/Context.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 import { content as Translations } from './data/Content';
+import { useLocalStorage } from './helpers/useLocalStorage';
 
 export const Context = React.createContext(null);
 
 export default function ContextProvider({ children }) {
-  const [language, setLanguage] = useState('german');
-  const [languageButton, setLanguageButton] = useState('EN');
+  const [language, setLanguage] = useLocalStorage('Language', 'german');
+  const [languageButton, setLanguageButton] = useState(
+    language === 'german' ? 'EN' : 'DE'
+  );
 
   const content = Translations[language];
 
