d15559cf29fb49e3a04ac74afec7dcdf6d17bce3
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-07-27 01:29:53 +0200

    Remove unneeded data
    The data is now fetched from an api.

--- src/data/IE.js
+++
@@ -1,6 +0,0 @@
-export const ie = {
-  german:
-    '<div style="width: 100%; max-width: 1170px; margin: 0 auto; padding: 15px"><h1>Achtung! Internet Explorer wird nicht mehr unterstützt.</h1><p>Unterstützt werden z.B. Chrome, Firefox, Safari, Edge und Opera.</p></div>',
-  english:
-    '<div style="width: 100%; max-width: 1170px; margin: 0 auto; padding: 15px"><h1>Attention! Internet Explorer is no longer supported.</h1><p>The following browsers are supported: e.g. Chrome, Firefox, Safari, Edge and Opera.</p></div>',
-};
